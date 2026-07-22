d86c0524d78afdc6a4d7fa03c732be69efab7977
fiwswe <53953985+fiwswe@users.noreply.github.com>
2026-07-22 18:34:58 +0200

    Add German language file for localization

---
+++ lang/de/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * german language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     fiwswe <>
+ */
+
+$lang['line']       = 'Zeile';
+$lang['lines']      = 'Zeilen';
+$lang['linesto']    = 'bis';
