ba3821f406f536d04e48166e8aab30231036740e
fiwswe <53953985+fiwswe@users.noreply.github.com>
2026-07-22 18:34:10 +0200

    Create lang.php for English language support
    Add English language file with basic translations.

---
+++ lang/en/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * english language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     fiwswe <>
+ */
+
+$lang['line']       = 'Line';
+$lang['lines']      = 'Lines';
+$lang['linesto']    = 'to';
